5eaab8ed7dca425c6aa288ef3660a30a706ab77e
tsrl <Strelnikovatatyana@gmail.com>
2020-08-04 00:53:22 +0200

    Project1 added

--- myrobot/build/CMakeFiles/Makefile.cmake
+++ myrobot/build/CMakeFiles/Makefile.cmake
@@ -95,5 +95,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/hello.dir/DependInfo.cmake"
+  "CMakeFiles/welcome_message.dir/DependInfo.cmake"
   )
