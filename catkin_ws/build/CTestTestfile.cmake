dd1f2b940cf4611bef935a8555698c51f16ed26f
tsrl <Strelnikovatatyana@gmail.com>
2020-08-04 20:12:00 +0200

    roslaunch error

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tastr/Udacity_Robostics/catkin_ws/src
+# Build directory: /home/tastr/Udacity_Robostics/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("my_robot")
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/tastr/Udacity_Robostics/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/tastr/Udacity_Robostics/catkin_ws/devel".split(';') if "/home/tastr/Udacity_Robostics/catkin_ws/devel" != "" else []
