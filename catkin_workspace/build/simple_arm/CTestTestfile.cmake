6e2daa09e7888da3afdfa494b0a0061f73b74ade
tastr <strelnikovatatyana@gmail.com>
2020-10-18 07:17:53 -0700

    del: catkin_ws with lecture 3 add: catkin_ws with lecture 4 	new file:   Lec4_catkin_ws/build/CMakeFiles/3.5.1/CMakeDetermineCompilerABI_C.bin

--- catkin_workspace/build/simple_arm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Udacity_Robotics/catkin_workspace/src/simple_arm
-# Build directory: /home/robond/Udacity_Robotics/catkin_workspace/build/simple_arm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
