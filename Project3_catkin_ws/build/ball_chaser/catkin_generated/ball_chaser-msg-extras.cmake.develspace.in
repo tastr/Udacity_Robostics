4c9957b6d576a6ee024fe5651889c2f2bf5e3674
tastr <strelnikovatatyana@gmail.com>
2020-10-23 06:59:11 -0700

    del: delete old projects

--- Project3_catkin_ws/build/ball_chaser/catkin_generated/ball_chaser-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/robond/Udacity_Robotics/Project3_catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
--- old_catkin_workspace/build/first_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Udacity_Robotics/catkin_workspace/src/first_package
-# Build directory: /home/robond/Udacity_Robotics/catkin_workspace/build/first_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
