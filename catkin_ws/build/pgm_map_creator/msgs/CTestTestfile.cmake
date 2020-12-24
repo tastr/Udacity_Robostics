8984993c9e604469b8dcaaf96082dabb36eee092
tastr <strelnikovatatyana@gmail.com>
2020-12-24 07:07:54 -0800

    ERROR: cannot launch node of type [pick_objects/pick_objects]

--- catkin_ws/build/pgm_map_creator/msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Udacity_Robotics/catkin_ws/src/pgm_map_creator/msgs
-# Build directory: /home/robond/Udacity_Robotics/catkin_ws/build/pgm_map_creator/msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
