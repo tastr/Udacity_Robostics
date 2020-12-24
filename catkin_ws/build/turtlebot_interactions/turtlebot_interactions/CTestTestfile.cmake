62bfd70c1d44074893c672145720c3a4293fa1b0
tastr <strelnikovatatyana@gmail.com>
2020-12-24 03:53:26 -0800

    feat: begin project 5

---
+++ catkin_ws/build/turtlebot_interactions/turtlebot_interactions/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Udacity_Robotics/catkin_ws/src/turtlebot_interactions/turtlebot_interactions
+# Build directory: /home/robond/Udacity_Robotics/catkin_ws/build/turtlebot_interactions/turtlebot_interactions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
